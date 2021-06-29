69a016b69bc4ec5fa0e3d1d87d322252fc892289
David McKay <david@rawkode.com>
2021-06-29 15:45:01 +0100

    feat: add netlify config

---
+++ websites/api/functions/api.js
@@ -0,0 +1,16 @@
+const fetch = require("sync-fetch");
+const jsonGraphQlServer = require("json-graphql-server");
+
+const data = fetch(
+  "https://github.com/cncf/cloudnativetv/releases/download/blox/data.json"
+).json();
+
+const app = require("express")();
+
+app.use("/", jsonGraphQlServer.default(data));
+
+const port = process.env.PORT || 4000;
+
+module.exports = app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
