0e242cd3bd3583bd8e12586bd14184c083e7a3be
David McKay <david@rawkode.com>
2021-06-29 15:54:03 +0100

    feat: use serverless handler

--- websites/api/functions/api.js
+++ websites/api/functions/api.js
@@ -1,5 +1,6 @@
 const fetch = require("sync-fetch");
 const jsonGraphQlServer = require("json-graphql-server");
+const serverless = require("serverless-http");
 
 const data = fetch(
   "https://github.com/cncf/cloudnativetv/releases/download/blox/data.json"
@@ -11,6 +12,4 @@ app.use("/", jsonGraphQlServer.default(data));
 
 const port = process.env.PORT || 4000;
 
-exports.handler = app.listen(port, () =>
-  console.log(`Server running on ${port}, http://localhost:${port}`)
-);
+module.exports.handler = serverless(app);
